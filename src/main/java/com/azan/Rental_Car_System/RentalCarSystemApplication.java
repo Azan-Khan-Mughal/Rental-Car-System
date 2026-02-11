b12765b7546eff5bdd2dd329c8cfd9f8196101d4
Azan Khan <khanazanmughal@gmail.com>
2026-02-11 16:06:56 +0500

    first commit

---
+++ src/main/java/com/azan/Rental_Car_System/RentalCarSystemApplication.java
@@ -0,0 +1,13 @@
+package com.azan.Rental_Car_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarSystemApplication.class, args);
+	}
+
+}
